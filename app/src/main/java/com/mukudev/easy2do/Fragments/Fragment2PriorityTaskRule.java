cdad835eeb25e5230cda49696c7077d260e642cd
RITIK CHAUDHRY <ritikchaudhry1305@gmail.com>
2024-10-21 14:01:09 +0530

    Update Fragment2PriorityTaskRule.java
    i tried to mitigate all the issues that you have mentioned but there is one syntax error of closing brackets. i have reviewed code multiple time but won't able to get how to fix it. i believe after fixing that the issue will get resolved. please help me into that

--- app/src/main/java/com/mukudev/easy2do/Fragments/Fragment2PriorityTaskRule.java
+++ app/src/main/java/com/mukudev/easy2do/Fragments/Fragment2PriorityTaskRule.java
@@ -22,7 +22,7 @@ public class Fragment2PriorityTaskRule extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_2priority_task_rule, container, false);
+View view = inflater.inflate(R.layout.fragment_2priority_task_rule, container, false);
 
         EditText task1Input = view.findViewById(R.id.task1_input);
         EditText task2Input = view.findViewById(R.id.task2_input);
@@ -32,8 +32,8 @@ public class Fragment2PriorityTaskRule extends Fragment {
         SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
         String task1 = sharedPreferences.getString("task1", "");
         String task2 = sharedPreferences.getString("task2", "");
-        boolean task1Completed = sharedPreferences.getBoolean("task1 Completed", false);
-        boolean task2Completed = sharedPreferences.getBoolean("task2 Completed", false);
+boolean task1Completed = sharedPreferences.getBoolean("task1_Completed", false);
+boolean task2Completed = sharedPreferences.getBoolean("task2_Completed", false);
 
         task1Input.setText(task1);
         task2Input.setText(task2);
@@ -47,6 +47,28 @@ public class Fragment2PriorityTaskRule extends Fragment {
             task2Input.setEnabled(false);
             task2Checkbox.setEnabled(false);
         }
+        view.findViewById(R.id.save_button).setOnClickListener(v -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("task1", task1Input.getText().toString());
+            editor.putString("task2", task2Input.getText().toString());
+            editor.apply();
+        });
+        
+        Calendar calendar = Calendar.getInstance();
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = calendar.get(Calendar.MINUTE);
+        if (currentHour == 0 && currentMinute == 0) {
+            task1Input.setText("");
+            task2Input.setText("");
+            task1Checkbox.setChecked(false);
+            task2Checkbox.setChecked(false);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("task 1", "");
+            editor.putString("task 2", "");
+editor.putBoolean("task1_Completed", false);
+editor.putBoolean("task2_Completed", false);
+            editor.apply();
+        }
 
         task1Checkbox.setOnClickListener(v -> {
             if (task1Checkbox.isChecked()) {
@@ -58,27 +80,8 @@ public class Fragment2PriorityTaskRule extends Fragment {
                         task1Checkbox.setEnabled(false);
                         showMotivationalMessage();
                         SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putBoolean("task1Completed", true);
-                        editor.putString("task1", task1Input.getText().toString());
-                        editor.apply();
-                    })
-                    .setNegativeButton("No", (dialog, which) -> task1Checkbox.setChecked(false))
-                    .show();
-            }
-        });
-
-        task2Checkbox.setOnClickListener(v -> {
-            if (task2Checkbox.isChecked()) {
-                new AlertDialog.Builder(getContext())
-                    .setTitle("Confirm")
-                    .setMessage("Are you sure you want to mark this task as completed?")
-                    .setPositiveButton("Yes", (dialog, which) -> {
-                        task2Input.setEnabled(false);
-                        task2Checkbox.setEnabled(false);
-                        showMotivationalMessage();
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putBoolean("task2Completed", true);
-                        editor.putString("task2", task2Input.getText().toString());
+editor.putBoolean("task1_Completed", true);
+editor.putBoolean("task2_Completed", true);
                         editor.apply();
                     })
                     .setNegativeButton("No", (dialog, which) -> task2Checkbox.setChecked(false))
@@ -86,51 +89,15 @@ public class Fragment2PriorityTaskRule extends Fragment {
             }
         });
 
+        return view;
+    }
 
+    private void showMotivationalMessage() {
+        String[] messages = getResources().getStringArray(R.array.motivational_messages);
+        Random random = new Random();
+        String message = messages[random.nextInt(messages.length)];
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
 
-        task1Checkbox.setOnClickListener(v -> {
-            if (task1Checkbox.isChecked()) {
-                new AlertDialog.Builder(getContext())
-                    .setTitle("Confirm")
-                    .setMessage("Are you sure you want to mark this task as completed?")
-                    .setPositiveButton("Yes", (dialog, which) -> {
-                        task1Input.setEnabled(false);
-                        task1Checkbox.setEnabled(false);
-                        showMotivationalMessage();
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putBoolean("task 1 Completed", true);
-                        editor.apply();
-                    })
-                    .setNegativeButton("No", (dialog, which) -> task1Checkbox.setChecked(false))
-                    .show();
-            }
-        });
-
-        task2Checkbox.setOnClickListener(v -> {
-            if (task2Checkbox.isChecked()) {
-                new AlertDialog.Builder(getContext())
-                    .setTitle("Confirm")
-                    .setMessage("Are you sure you want to mark this task as completed?")
-                    .setPositiveButton("Yes", (dialog, which) -> {
-                        task2Input.setEnabled(false);
-                        task2Checkbox.setEnabled(false);
-                        showMotivationalMessage();
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putBoolean("task 2 Completed", true);
-                        editor.apply();
-                    })
-                    .setNegativeButton("No", (dialog, which) -> task2Checkbox.setChecked(false))
-                    .show();
-            }
-        });
-
-         return view;
-     }
-
-     private void showMotivationalMessage() {
-         String[] messages = getResources().getStringArray(R.array.motivational_messages);
-         Random random = new Random();
-         String message = messages[random.nextInt(messages.length)];
-         Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
-     }
- }
+    }
+}
